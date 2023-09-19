babe9b03e2dcd16da866ff817769ed7e46e99cd6
keastar <keastar1986@gmail.com>
2023-09-19 16:44:02 +0300

    done task 5!
    done task 5!

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,39 +1,54 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { ImSearch } from 'react-icons/im';
 import css from './Movies.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { searchMovie } from '../api/API_KEY';
+import { useLocation, useSearchParams, Link } from 'react-router-dom';
 
-export default function Movies({ onSubmit }) {
-  const [searchMvName, setSearchMvName] = useState('');
-  const handleNameChange = event => {
-    setSearchMvName(event.currentTarget.value.toLowerCase());
-  };
+export default function Movies() {
+  const [moviesFind, setMoviesFind] = useState([]);
+  const [noFilms, setNoFilms] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('search');
+  const location = useLocation();
+
   const handleSubmit = event => {
     event.preventDefault();
-    if (searchMvName.trim() === '') {
-      return alert('Введите, что хотите найти из фильмов!');
-    }
-    //2.И передаю в метод значение из этого файла state=searchImgName -> прокидываем пропс с App 'propName' и передаем ему значение с Searchbar -> searchImgName
-    onSubmit(searchMvName);
-    //5.Идет очистка формы поля input после ее сабмита
-    setSearchMvName('');
+
+    const form = event.currentTarget;
+    const searchQuery = form.elements.search.value;
+
+    setSearchParams({ search: searchQuery });
+    form.reset();
   };
+
+  useEffect(() => {
+    setNoFilms(false);
+    if (!query) return;
+
+    (async function searchMovies() {
+      const res = await searchMovie(query);
+      if (res.data.results.lenght === 0) {
+        setNoFilms(true);
+        return;
+      }
+      setMoviesFind(res.data.results);
+    })(query);
+
+    return () => {};
+  }, [query]);
 
   return (
     <>
       <div className={css.movies}>
-        {/* // 1.при событии Submit формы -> вызываю из App -> handleSubmit -> propName */}
         <form className={css.form} onSubmit={handleSubmit}>
           <input
             className={css.input}
             type="text"
-            name="searchMvName"
-            value={searchMvName}
-            onChange={handleNameChange}
+            name="search"
             autoComplete="off"
             autoFocus
-            placeholder="Search images and photos"
+            placeholder="Search movies"
           />
           <button type="submit" className={css.button}>
             <ImSearch
@@ -45,12 +60,23 @@ export default function Movies({ onSubmit }) {
           </button>
         </form>
       </div>
+      {query && !noFilms && (
+        <ul className={css.ul}>
+          {moviesFind.map(({ id, name, title }) => (
+            <li key={id}>
+              <Link
+                // className={css.li}
+                key={id}
+                to={`${id}`}
+                state={{ from: location }}
+              >
+                {title} {name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
       <div className={css.page_this}>!MOVIES PAGE!</div>
     </>
   );
 }
-// export default Movies;
-
-// Movies.propTypes = {
-// onSubmit: PropTypes.func.isRequired,
-// };
